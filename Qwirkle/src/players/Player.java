5aca923655cefaeab27da6cf4750b980583290b4
Justin Altena <psvjustin@gmail.com>
2016-01-18 11:53:01 +0100

    player aangepast

--- Qwirkle/src/players/Player.java
+++ Qwirkle/src/players/Player.java
@@ -1,43 +1,50 @@
 package players;
 
+import game.Bag;
 import game.Board;
 import game.Stone;
+import game.Location;
 
 public class Player {
 	private Board board;
 	private String name;
 	private Stone[] hand;
+	private int points;
 	
 	public Player(Board board, String name) {
 		this.board = board;
 		this.name = name;
+		points = 0;
 	}
 	
+	//Wat is de bedoeling van MakeMove?
 	public void makeMove(){
-	test345;
-	}
-	
-	public void take(){
-		hallo;
-	}
-	
-	public void place(){
 		
 	}
 	
+	public void take(){
+		Bag.takeStone();
+	}
+	
+	public void place(Stone stone,) {
+		board.setField(int x, int y, Stone stone);
+	}
+	
 	public String trade(){
-		return null;
+		Bag.tradeStone(Stone[] stones) {
+		}
 		
 	}
 	
 	public void addPoints(int newPoints){
+		this.points = this.points + newPoints;
 		
 		
 	}
 			
 	
 	public int getPoints(){
-		return 0;
+		return points;
 		
 	}
 
